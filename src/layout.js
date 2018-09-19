fb7fef16ead6637cfc36dbb9f4180bfb1a315387
lichengyin <lichengyin@360.cn>
2018-09-19 12:04:18 +0800

    remove right & bottom property

--- src/layout.js
+++ src/layout.js
@@ -1,15 +1,13 @@
 class Layout {
-  constructor(left, right, top, bottom, width, height) {
+  constructor(left, top, width, height) {
     this.left = left || 0;
-    this.right = right || 0;
     this.top = top || 0;
-    this.bottom = bottom || 0;
     this.width = width || 0;
     this.height = height || 0;
   }
 
   toString() {
-    return `<Layout#${this.left}:${this.right};${this.top}:${this.bottom};${
+    return `<Layout#${this.left};${this.top};${
       this.width
     }:${this.height}>`;
   }
--- src/node.js
+++ src/node.js
@@ -18,7 +18,7 @@ class Node {
   }
 
   set parent(node) {
-    this.parent = node;
+    this[PARENT] = node;
     this.config.container = node;
   }
 
@@ -37,7 +37,7 @@ class Node {
   }
 
   getComputedLayout() {
-    const layout = new Layout(this.left, this.right, this.top, this.bottom, this.width, this.height);
+    const layout = new Layout(this.left, this.top, this.width, this.height);
     return layout;
   }
 
